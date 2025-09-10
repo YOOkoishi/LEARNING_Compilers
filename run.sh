68ef5f46306fa6e6a551ffa9a2ab78234b007a4a
YOO_koishi <2358181935@qq.com>
2025-09-10 19:36:47 +0800

    ✨feat: Add run.sh

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -it --rm -v $(pwd):/root/compiler maxxing/compiler-dev bash -c "
+    cd compiler &&
+    cmake -DCMAKE_BUILD_TYPE=Debug -B build &&
+    cmake --build build &&
+    cd build &&
+    bash
+"
