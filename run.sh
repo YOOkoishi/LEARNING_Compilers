d37fe5ac89da04ac93831849dd22cbb95ab0850c
YOO_koishi <2358181935@qq.com>
2025-09-14 16:14:20 +0800

    🔧fix: 修改run.sh启动docker为sudo模式

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run -it --rm -v $(pwd):/root/compiler maxxing/compiler-dev bash -c "
+sudo docker run -it --rm -v $(pwd):/root/compiler maxxing/compiler-dev bash -c "
     cd compiler &&
     cmake -DCMAKE_BUILD_TYPE=Debug -B build &&
     cmake --build build &&
